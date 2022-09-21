67412839cf1a57c029c7c0960bfa341bdc493334
vuxnq <vuxnqu@gmail.com>
2022-09-21 16:34:05 +0200

    added enter-able shop interior

--- scripts/shop.gd
+++ scripts/shop.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+export(PackedScene) var interior
+
 onready var player = get_parent().get_node("Player")
 
 func _ready():
@@ -7,4 +9,7 @@ func _ready():
 
 func _on_EnterArea_body_entered(body):
 	if body == player:
-		print("Entering Da Shop")
+		Global.player_position = body.global_position
+		Global.player_position.y += 50
+# warning-ignore:return_value_discarded
+		get_tree().change_scene_to(interior)
