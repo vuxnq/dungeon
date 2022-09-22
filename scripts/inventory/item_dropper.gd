5ad7a566ab97651f100d7bbab13901595b5c4086
vuxnq <vuxnqu@gmail.com>
2022-09-22 10:47:48 +0200

    added pickup-able item drops

---
+++ scripts/inventory/item_dropper.gd
@@ -0,0 +1,13 @@
+extends Area2D
+var item_drop = preload("res://scenes/gui/inventory/item_drop.tscn")
+var drop = preload("res://resources/diamond.tres") # actual drop (like diamonds)
+
+
+func _on_DropArea_body_entered(body):
+	drop_item(drop)
+
+func drop_item(drop):
+	var item_drop_instance = item_drop.instance()
+	item_drop_instance.item = drop
+	item_drop_instance.position = position
+	get_parent().call_deferred("add_child", item_drop_instance)
